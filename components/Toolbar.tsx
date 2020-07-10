118c6777e8c3ea243fd2556bda859f04baf48c52
Tristan Grovender <therealtristangrovender@gmail.com>
2020-07-10 13:13:57 -0500

    Working on toolbar styling

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
 const ToolbarContainer = styled.div`
-  background-color: blue;
+  background-color: #222;
+  height: 80px;
+  color: #ccc;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ToolbarContent = styled.div`
+  width: 960px;
+  align-items: center;
 `;
 
 export default function Toolbar() {
-  return <ToolbarContainer>Toolbar</ToolbarContainer>;
+  return (
+    <ToolbarContainer>
+      <ToolbarContent>Toolbar</ToolbarContent>
+    </ToolbarContainer>
+  );
 }
