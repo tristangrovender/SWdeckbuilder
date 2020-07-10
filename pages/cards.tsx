5faad7b1188b44de30f81da13e821bcf721fe541
Daniel Rasmuson <dan123911@gmail.com>
2020-07-10 17:28:00 -0500

    Adding a name filter

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -4,21 +4,29 @@ import { LightCards } from "../cards/Light";
 import { DarkCards } from "../cards/Dark";
 
 export default function Cards() {
-  const [test, setTest] = useState([]);
+  const [nameFilter, setNameFilter] = useState(null);
   console.log("woohoo hooks are working!");
   return (
     <div>
       <Toolbar />
       <Content>
-        {LightCards.slice(0, 30).map((card) => {
-          return (
-            <img
-              src={card.front.imageUrl}
-              key={card.id}
-              style={{ height: "330px", display: "inline", margin: "10px" }}
-            ></img>
-          );
-        })}
+        <input onKeyUp={(e) => setNameFilter(e.target.value)}></input>
+        {LightCards.filter((card) => {
+          if (!nameFilter) {
+            return true;
+          }
+          return card.front.title.includes(nameFilter);
+        })
+          .slice(0, 30)
+          .map((card) => {
+            return (
+              <img
+                src={card.front.imageUrl}
+                key={card.id}
+                style={{ height: "330px", display: "inline", margin: "10px" }}
+              ></img>
+            );
+          })}
       </Content>
     </div>
   );
