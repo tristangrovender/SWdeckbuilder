13dbb6779ec515dce907fa07a4b0a53c4b6b04df
Daniel Rasmuson <dan123911@gmail.com>
2020-07-10 14:22:01 -0500

    Rendering cards

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -1,10 +1,23 @@
 import { Toolbar, Content } from "../components/Toolbar";
+import { LightCards } from "../cards/Light";
+import { DarkCards } from "../cards/Dark";
 
 export default function Cards() {
+  console.log(LightCards.length);
   return (
     <div>
       <Toolbar />
-      <Content>cards</Content>
+      <Content>
+        {[...LightCards, ...DarkCards].slice(0, 30).map((card) => {
+          return (
+            <img
+              src={card.front.imageUrl}
+              key={card.id}
+              style={{ height: "330px", display: "inline" }}
+            ></img>
+          );
+        })}
+      </Content>
     </div>
   );
 }
