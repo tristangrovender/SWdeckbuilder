28a3b88d72394b3e85d9be3c53861ff6bab01911
Tristan Grovender <therealtristangrovender@gmail.com>
2020-07-10 18:29:01 -0500

    tiles partially updated

--- components/DeckTile.tsx
+++ components/DeckTile.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Tile = styled.div`
+  background-color: white;
   border: 3px solid red;
   color: white;
   width: 500px;
@@ -10,12 +11,35 @@ const Tile = styled.div`
 const TileBanner = styled.div`
   height: 50px;
   background-color: #1d7a99;
+  font-size: 16px;
+  padding: 5px;
+`;
+
+const TileBannerAuthor = styled.div`
+  font-size: 10px;
+`;
+
+const TileBannerDeck = styled.div`
+  color: black;
+  font-size: 14px;
+`;
+
+const TileBannerDeckContents = styled.div`
+  color: black;
+  font-size: 14px;
 `;
 
 export function DeckTile() {
   return (
     <Tile>
-      <TileBanner>May the 4th be with you</TileBanner>
+      <TileBanner>
+        May the 4th be with you
+        <TileBannerAuthor>by darthvoodoo 2 · days ago</TileBannerAuthor>
+      </TileBanner>
+      <TileBannerDeck>Deck: Dark</TileBannerDeck>
+      <TileBannerDeckContents>
+        35 Characters • 10 Effects • 13 Weapons • 15 Interrupts • 3 Objectives
+      </TileBannerDeckContents>
     </Tile>
   );
 }
