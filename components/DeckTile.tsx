2b5b0608b026614a22ba58bfc33e1ecd71a0f75a
Tristan Grovender <therealtristangrovender@gmail.com>
2020-07-12 12:22:33 -0500

    Seeding cards

--- components/DeckTile.tsx
+++ components/DeckTile.tsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
+// import dark from "../public/images/dark.png";
 
 const Tile = styled.div`
   background-color: white;
-  border: 3px solid red;
+  /* border: 3px solid red; */
   color: white;
   width: 500px;
-  height: 150px;
+  height: 125px;
+`;
+
+const TileBannerImage = styled.img`
+  height: 50px;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: #1d7a99;
+`;
+
+const TileBannerContainer = styled.div`
+  display: flex;
 `;
 
 const TileBanner = styled.div`
@@ -13,33 +25,42 @@ const TileBanner = styled.div`
   background-color: #1d7a99;
   font-size: 16px;
   padding: 5px;
+  width: 100%;
 `;
 
-const TileBannerAuthor = styled.div`
+const TileAuthor = styled.div`
   font-size: 10px;
 `;
 
-const TileBannerDeck = styled.div`
+const TileDescription = styled.div`
   color: black;
   font-size: 14px;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
 
-const TileBannerDeckContents = styled.div`
+const TileDeckContents = styled.div`
   color: black;
   font-size: 14px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
 
 export function DeckTile({
+  img,
   title,
   author,
   days,
-  deck,
+  description,
   cardBreakdown
 }: {
+  img: string;
   title: string;
   author: string;
   days: number;
-  deck: string;
+  description: string;
   cardBreakdown: {
     characters: number;
     effects: number;
@@ -50,18 +71,21 @@ export function DeckTile({
 }) {
   return (
     <Tile>
-      <TileBanner>
-        {title}
-        <TileBannerAuthor>
-          by {author} · {days} days ago
-        </TileBannerAuthor>
-      </TileBanner>
-      <TileBannerDeck>Deck: {deck}</TileBannerDeck>
-      <TileBannerDeckContents>
+      <TileBannerContainer>
+        <TileBannerImage src={img} />
+        <TileBanner>
+          {title}
+          <TileAuthor>
+            by {author} · {days} days ago
+          </TileAuthor>
+        </TileBanner>
+      </TileBannerContainer>
+      <TileDescription>Description: {description}</TileDescription>
+      <TileDeckContents>
         {cardBreakdown.characters} Characters • {cardBreakdown.effects} Effects
         • {cardBreakdown.weapons} Weapons • {cardBreakdown.interrupts}{" "}
         Interrupts • {cardBreakdown.objectives} Objectives
-      </TileBannerDeckContents>
+      </TileDeckContents>
     </Tile>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,16 +32,87 @@ export default function Home() {
             Deckbuilder for the Star Wars Customizable Card Game
           </HomePageH2>
           <DeckTile
+            img="/images/dark.png"
             title="May the 4th be with you"
             author="darthvoodoo"
             days={2}
-            deck="Dark"
+            description="Fast paced, high stakes, fun deck"
             cardBreakdown={{
               characters: 35,
               effects: 10,
               weapons: 13,
               interrupts: 15,
               objectives: 3
+            }}
+          />
+          <DeckTile
+            img="/images/light.png"
+            title="Princess leia's legion"
+            author="freeForce4you"
+            days={10}
+            description="Slow methodical deck good against many types"
+            cardBreakdown={{
+              characters: 45,
+              effects: 14,
+              weapons: 9,
+              interrupts: 5,
+              objectives: 7
+            }}
+          />
+          <DeckTile
+            img="/images/dark.png"
+            title="PLANET DESTROYER"
+            author="darthWillMaulYou"
+            days={7}
+            description="Weapons galore"
+            cardBreakdown={{
+              characters: 10,
+              effects: 10,
+              weapons: 37,
+              interrupts: 3,
+              objectives: 5
+            }}
+          />
+          <DeckTile
+            img="/images/light.png"
+            title="Solo's surprise"
+            author="Falconator"
+            days={15}
+            description="Fast paced, high stakes, fun deck"
+            cardBreakdown={{
+              characters: 33,
+              effects: 11,
+              weapons: 14,
+              interrupts: 9,
+              objectives: 5
+            }}
+          />
+          <DeckTile
+            img="/images/light.png"
+            title="Princess leia's legion"
+            author="freeForce4you"
+            days={10}
+            description="Slow methodical deck good against many types"
+            cardBreakdown={{
+              characters: 45,
+              effects: 14,
+              weapons: 9,
+              interrupts: 5,
+              objectives: 7
+            }}
+          />
+          <DeckTile
+            img="/images/dark.png"
+            title=""
+            author="darthWillMaulYou"
+            days={7}
+            description="Weapons galore"
+            cardBreakdown={{
+              characters: 10,
+              effects: 10,
+              weapons: 37,
+              interrupts: 3,
+              objectives: 5
             }}
           />
         </HomePageContent>
