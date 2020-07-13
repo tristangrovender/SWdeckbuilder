09042b6be1a0be7d8d4e787358af1ce84d45aac0
Daniel Rasmuson <dan123911@gmail.com>
2020-07-13 16:46:33 -0500

    Adding set filter

--- components/card-search-table/card-filters-bar.tsx
+++ components/card-search-table/card-filters-bar.tsx
@@ -1,5 +1,7 @@
 import SearchIcon from "@material-ui/icons/Search";
 import styled from "styled-components";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const Input = styled.input`
   background-color: transparent;
@@ -19,6 +21,7 @@ export function CardFiltersBar() {
         backgroundColor: "#2d2d2f",
         padding: "10px",
         color: "white",
+        alignItems: "center",
       }}
     >
       <div
@@ -29,10 +32,34 @@ export function CardFiltersBar() {
           alignItems: "center",
           justifyContent: "center",
           padding: "3px 7px",
+          marginRight: "10px",
         }}
       >
         <SearchIcon style={{ transform: "rotate(90deg)" }} />
         <Input placeholder="Search"></Input>
+      </div>
+      <div
+        style={{
+          borderRadius: "50px",
+          border: "1px solid #6f6f6f",
+          display: "flex",
+          alignItems: "center",
+          paddingRight: "10px",
+          justifyContent: "center",
+        }}
+      >
+        <MenuBookIcon
+          style={{
+            fontSize: "30px",
+            marginRight: "5px",
+            borderRadius: "100px",
+            border: "1px solid white",
+            padding: "5px",
+          }}
+        />
+        <div style={{ marginLeft: "3px" }}>Set: All</div>
+
+        <ExpandMoreIcon style={{ marginLeft: "5px", fontSize: "16px" }} />
       </div>
     </div>
   );
