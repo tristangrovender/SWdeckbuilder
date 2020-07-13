60f969dc7f5459fac4b6d7a39399864bacd2fd3c
Daniel Rasmuson <dan123911@gmail.com>
2020-07-13 11:08:55 -0500

    Got card aadding working

--- pages/deck/edit/[id].tsx
+++ pages/deck/edit/[id].tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { Page, Toolbar, Content } from "../../../components/Toolbar";
 import { useRouter } from "next/router";
 import { CardSearchTable } from "../../../components/card-search-table/card-search-table";
-import { Side } from "../../../components/card-search-table/card.interface";
+import {
+  Side,
+  Card,
+} from "../../../components/card-search-table/card.interface";
 
-function CardPanel() {
+function CardPanel({ cards }: { cards: Card[] }) {
   return (
     <div style={{ width: "300px", height: "400px", border: "1px solid red" }}>
       Card Panel
+      {cards.map((card, i) => (
+        <div key={i}>{card.front.title}</div>
+      ))}
     </div>
   );
 }
 
 export default function EditDeck(params) {
   const router = useRouter();
+  const [deckCards, setDeckCards] = useState([]);
+  const addCard = (card: Card) => {
+    setDeckCards([...deckCards, card]);
+  };
   const { id: deckId } = router.query;
   if (!deckId) {
     return (
@@ -31,11 +42,8 @@ export default function EditDeck(params) {
         <div contentEditable={true}>Un-named Deck</div>
         <div style={{ display: "flex" }}>
           {/* TODO showSide will need to come from /deck/new choice */}
-          <CardSearchTable
-            showSide={Side.dark}
-            onCardSelected={(card) => console.log("card selected", card)}
-          />
-          <CardPanel></CardPanel>
+          <CardSearchTable showSide={Side.dark} onCardSelected={addCard} />
+          <CardPanel cards={deckCards}></CardPanel>
         </div>
       </Content>
     </Page>
