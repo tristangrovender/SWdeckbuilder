b24108351db2ac1e612c0445aa1ebd37320b03b8
Daniel Rasmuson <dan123911@gmail.com>
2020-07-14 15:07:22 -0500

    Added filter bar to cards screen

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { Toolbar, Content, Page } from "../components/Toolbar";
 import { CardSearchTable } from "../components/card-search-table/card-search-table";
+import { CardFiltersBar } from "../components/card-search-table/card-filters-bar";
 
 export default function Cards() {
+  const [filters, updateFilters] = useState(undefined);
   return (
     <Page>
       <Toolbar />
-      <Content>
-        <CardSearchTable />
-      </Content>
+      <CardFiltersBar
+        filters={filters}
+        onUpdateFilters={(filters) => updateFilters(filters)}
+      />
+      <CardSearchTable filters={filters} />
     </Page>
   );
 }
