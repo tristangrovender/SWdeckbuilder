1f88643100a8a2e20d327d9d195547a8613e96e4
Daniel Rasmuson <dan123911@gmail.com>
2020-07-14 11:30:23 -0500

    Added a bunch of filters

--- components/card-search-table/card-filters-bar.tsx
+++ components/card-search-table/card-filters-bar.tsx
@@ -2,6 +2,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import styled from "styled-components";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 
 const Input = styled.input`
   background-color: transparent;
@@ -11,6 +12,36 @@ const Input = styled.input`
     outline: none;
   }
 `;
+
+function FilterIcon({ Icon, text }: { Icon: any; text: string }) {
+  return (
+    <div
+      style={{
+        borderRadius: "50px",
+        border: "1px solid #6f6f6f",
+        display: "flex",
+        alignItems: "center",
+        padding: "2px 0px",
+        paddingRight: "10px",
+        justifyContent: "center",
+        marginRight: "10px",
+      }}
+    >
+      <Icon
+        style={{
+          fontSize: "30px",
+          marginRight: "5px",
+          borderRadius: "100px",
+          border: "1px solid white",
+          padding: "5px",
+        }}
+      />
+      <div style={{ marginLeft: "3px" }}>{text}</div>
+
+      <ExpandMoreIcon style={{ marginLeft: "5px", fontSize: "16px" }} />
+    </div>
+  );
+}
 
 export function CardFiltersBar() {
   return (
@@ -38,30 +69,12 @@ export function CardFiltersBar() {
         <SearchIcon style={{ transform: "rotate(90deg)" }} />
         <Input placeholder="Search"></Input>
       </div>
-      <div
-        style={{
-          borderRadius: "50px",
-          border: "1px solid #6f6f6f",
-          display: "flex",
-          alignItems: "center",
-          padding: "2px 0px",
-          paddingRight: "10px",
-          justifyContent: "center",
-        }}
-      >
-        <MenuBookIcon
-          style={{
-            fontSize: "30px",
-            marginRight: "5px",
-            borderRadius: "100px",
-            border: "1px solid white",
-            padding: "5px",
-          }}
-        />
-        <div style={{ marginLeft: "3px" }}>Set: All</div>
-
-        <ExpandMoreIcon style={{ marginLeft: "5px", fontSize: "16px" }} />
-      </div>
+      <FilterIcon Icon={MenuBookIcon} text={"Set: All"} />
+      <FilterIcon Icon={SupervisorAccountIcon} text={"Type: All"} />
+      <FilterIcon Icon={SupervisorAccountIcon} text={"Destiny: All"} />
+      <FilterIcon Icon={SupervisorAccountIcon} text={"Power: All"} />
+      <FilterIcon Icon={SupervisorAccountIcon} text={"Deploy: All"} />
+      <FilterIcon Icon={SupervisorAccountIcon} text={"Forfeit: All"} />
     </div>
   );
 }
