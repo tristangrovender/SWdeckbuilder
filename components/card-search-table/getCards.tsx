b61441f8817d573bc9347279b54d8dbbb6acc26a
Daniel Rasmuson <dan123911@gmail.com>
2020-07-15 09:34:51 -0500

    Added a memoize to getCards

--- components/card-search-table/getCards.tsx
+++ components/card-search-table/getCards.tsx
@@ -1,3 +1,20 @@
+async function loadCards() {
+  const cards = (await import("../../cards/cards.json")).default;
+  return cards;
+}
+
+function memoize<T>(func: Function) {
+  let result;
+  return (...args: T[]) => {
+    if (!result) {
+      result = func();
+    }
+    return result;
+  };
+}
+
+const loadCardsOnce = memoize(loadCards);
+
 export async function getCards() {
-  return (await import("../../cards/cards.json")).default;
+  return loadCardsOnce();
 }
