2578b39f9d3fa207944d724344291066c7b99fae
Daniel Rasmuson <dan123911@gmail.com>
2020-07-15 09:38:58 -0500

    Removed legacy cards

--- components/card-search-table/getCards.tsx
+++ components/card-search-table/getCards.tsx
@@ -1,6 +1,10 @@
+import { Card } from "./card.interface";
+
 async function loadCards() {
-  const cards = (await import("../../cards/cards.json")).default;
-  return cards;
+  const cards = (await import("../../cards/cards.json")).default as Card[];
+  return cards.filter(({ legacy }) => {
+    return legacy === false;
+  });
 }
 
 function memoize<T>(func: Function) {
