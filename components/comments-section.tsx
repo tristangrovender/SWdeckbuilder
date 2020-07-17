113fc9730a850dd24c01eeea92728e76d964e405
Daniel Rasmuson <dan123911@gmail.com>
2020-07-17 12:45:35 -0500

    Working on comments component

--- components/comments-section.tsx
+++ components/comments-section.tsx
@@ -3,35 +3,54 @@ import { Button } from "@material-ui/core";
 function Comment({
   author,
   text,
+  profilePhoto,
   createdAt,
 }: {
   author: string;
   text: string;
+  profilePhoto: string;
   createdAt: Date;
 }) {
   return (
-    <div>
-      {text}
-      {author}
-      {createdAt.toString()}
+    <div style={{ display: "flex" }}>
+      <img src={profilePhoto} style={{ width: "30px" }}></img>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex" }}>
+          <div style={{ textDecoration: "bold" }}> {author}</div>
+          <div style={{ color: "grey" }}>{createdAt.toString()}</div>
+        </div>
+
+        <div>{text}</div>
+      </div>
     </div>
   );
 }
 
 export function CommentsSection() {
   const comments = [
-    { author: "Dan Rasmuson", text: "Fantastic!", createdAt: new Date() },
+    {
+      author: "Dan Rasmuson",
+      text: "Fantastic!",
+      createdAt: new Date(),
+      profilePhoto: "/images/dark.png",
+    },
     {
       author: "Tristan Grovender",
       text: "Really effective!",
       createdAt: new Date(),
+      profilePhoto: "/images/dark.png",
     },
   ];
   return (
     <div>
       <div style={{ borderBottom: "1px solid lightgrey" }}>Comments</div>
-      {comments.map(({ author, text, createdAt }) => (
-        <Comment author={author} text={text} createdAt={createdAt}></Comment>
+      {comments.map(({ author, text, createdAt, profilePhoto }) => (
+        <Comment
+          author={author}
+          text={text}
+          createdAt={createdAt}
+          profilePhoto={profilePhoto}
+        ></Comment>
       ))}
       <textarea></textarea>
       <Button
