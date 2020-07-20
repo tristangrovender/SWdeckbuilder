d2e74f4d55a7faf8d3a0a728d6ca78f7433cfd93
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-20 11:47:38 -0500

    Moved deck text to its own file

---
+++ components/getDeckText.tsx
@@ -0,0 +1,64 @@
+import { Card } from "./card-search-table/card.interface";
+export function getDeckText(deck: Card[]): string {
+  const getTitle = (card: Card) => card.front.title;
+  const [
+    characterTitles,
+    objectiveTitles,
+    locationTitles,
+    creatureTitles,
+    weaponTitles,
+    deviceTitles,
+    starshipTitles,
+    vehicleTitles,
+    effectTitles,
+    interruptTitles,
+    epicEventTitles,
+    podracerTitles,
+    admiralsOrdersTitles,
+  ] = [
+    "Character",
+    "Objective",
+    "Location",
+    "Creature",
+    "Weapon",
+    "Device",
+    "Starship",
+    "Vehicle",
+    "Effect",
+    "Interrupt",
+    "Epic Event",
+    "Podracer",
+    "Admiral's Order",
+  ].map((type) => {
+    return deck.filter((card) => card.front.type === type).map(getTitle);
+  });
+  const jediTestTitles = deck
+    .filter((card) => card.front.type.includes("Jedi Test"))
+    .map(getTitle);
+  const sideDeckTitles = deck
+    .filter(
+      (card) =>
+        card.front.type === "Defensive Shield" || card.front.type === "Game Aid"
+    )
+    .map(getTitle);
+
+  return `Objectives: (${objectiveTitles.length})\n${objectiveTitles.join(
+    "\n"
+  )}\n
+Locations: (${locationTitles.length})\n${locationTitles.join("\n")}\n
+Characters: (${characterTitles.length})\n${characterTitles.join("\n")}\n
+Creatures: (${creatureTitles.length})\n${creatureTitles.join("\n")}\n
+Weapons: (${weaponTitles.length})\n${weaponTitles.join("\n")}\n
+Devices: (${deviceTitles.length})\n${deviceTitles.join("\n")}\n
+Starships: (${starshipTitles.length})\n${starshipTitles.join("\n")}\n
+Vehicles: (${vehicleTitles.length})\n${vehicleTitles.join("\n")}\n
+Effects: (${effectTitles.length})\n${effectTitles.join("\n")}\n
+Interrupts: (${interruptTitles.length})\n${interruptTitles.join("\n")}\n
+Epic Events: (${epicEventTitles.length})\n${epicEventTitles.join("\n")}\n
+Jedi Tests: (${jediTestTitles.length})\n${jediTestTitles.join("\n")}\n
+Admiral's orders: (${admiralsOrdersTitles.length})\n${admiralsOrdersTitles.join(
+    "\n"
+  )}\n
+Podracers: (${podracerTitles.length})\n${podracerTitles.join("\n")}\n
+Side Deck: (${sideDeckTitles.length})\n${sideDeckTitles.join("\n")}\n`;
+}
--- pages/deck/[id].tsx
+++ pages/deck/[id].tsx
@@ -8,11 +8,11 @@ import { darkBlue } from "../../utils/colors";
 import { groupBy } from "../../utils/utils";
 import { StarsComponent } from "../../components/StarsComponent";
 import { DeckCardRow } from "./DeckCardRow";
-import ImportExportIcon from "@material-ui/icons/ImportExport";
 import { CommentsSection } from "../../components/comments-section";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import { Button, ClickAwayListener } from "@material-ui/core";
 import FileSaver from "file-saver";
+import { getDeckText } from "../../components/getDeckText";
 
 function saveToFile(fileName: string, body: string) {
   var blob = new Blob([body], { type: "text/plain;charset=utf-8" });
@@ -155,70 +155,6 @@ function CardTypeSection({ cards }: { cards: Card[] }) {
       </div>
     </TypeContainer>
   );
-}
-
-function getDeckText(deck: Card[]): string {
-  const getTitle = (card: Card) => card.front.title;
-  const [
-    characterTitles,
-    objectiveTitles,
-    locationTitles,
-    creatureTitles,
-    weaponTitles,
-    deviceTitles,
-    starshipTitles,
-    vehicleTitles,
-    effectTitles,
-    interruptTitles,
-    epicEventTitles,
-    podracerTitles,
-    admiralsOrdersTitles,
-  ] = [
-    "Character",
-    "Objective",
-    "Location",
-    "Creature",
-    "Weapon",
-    "Device",
-    "Starship",
-    "Vehicle",
-    "Effect",
-    "Interrupt",
-    "Epic Event",
-    "Podracer",
-    "Admiral's Order",
-  ].map((type) => {
-    return deck.filter((card) => card.front.type === type).map(getTitle);
-  });
-  const jediTestTitles = deck
-    .filter((card) => card.front.type.includes("Jedi Test"))
-    .map(getTitle);
-  const sideDeckTitles = deck
-    .filter(
-      (card) =>
-        card.front.type === "Defensive Shield" || card.front.type === "Game Aid"
-    )
-    .map(getTitle);
-
-  return `Objectives: (${objectiveTitles.length})\n${objectiveTitles.join(
-    "\n"
-  )}\n
-Locations: (${locationTitles.length})\n${locationTitles.join("\n")}\n
-Characters: (${characterTitles.length})\n${characterTitles.join("\n")}\n
-Creatures: (${creatureTitles.length})\n${creatureTitles.join("\n")}\n
-Weapons: (${weaponTitles.length})\n${weaponTitles.join("\n")}\n
-Devices: (${deviceTitles.length})\n${deviceTitles.join("\n")}\n
-Starships: (${starshipTitles.length})\n${starshipTitles.join("\n")}\n
-Vehicles: (${vehicleTitles.length})\n${vehicleTitles.join("\n")}\n
-Effects: (${effectTitles.length})\n${effectTitles.join("\n")}\n
-Interrupts: (${interruptTitles.length})\n${interruptTitles.join("\n")}\n
-Epic Events: (${epicEventTitles.length})\n${epicEventTitles.join("\n")}\n
-Jedi Tests: (${jediTestTitles.length})\n${jediTestTitles.join("\n")}\n
-Admiral's orders: (${admiralsOrdersTitles.length})\n${admiralsOrdersTitles.join(
-    "\n"
-  )}\n
-Podracers: (${podracerTitles.length})\n${podracerTitles.join("\n")}\n
-Side Deck: (${sideDeckTitles.length})\n${sideDeckTitles.join("\n")}\n`;
 }
 
 export default function Deck() {
