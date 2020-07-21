f9ce6552cdd94dadfc3de4d092d95518c7126d3c
Tristan Grovender <therealtristangrovender@gmail.com>
2020-07-21 10:42:55 -0500

    Fixed alpha sort

--- components/card-search-table/getCards.tsx
+++ components/card-search-table/getCards.tsx
@@ -2,10 +2,15 @@ import { Card } from "./card.interface";
 import { memoize } from "../../utils/utils";
 
 function sortCardsByName(a: Card, b: Card) {
-  if (a.front.title < b.front.title) {
+  // update function so that the dots before the name are ignored
+
+  const aTitle = a.front.title.replace(/[^0-9a-zA-z_.]/gi, "");
+  const bTitle = b.front.title.replace(/[^0-9a-zA-z_.]/gi, "");
+
+  if (aTitle < bTitle) {
     return -1;
   }
-  if (a.front.title > b.front.title) {
+  if (aTitle > bTitle) {
     return 1;
   }
   return 0;
