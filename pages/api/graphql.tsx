cd4b328c4acdfb2380c7a66b1a5dd9b5526dd1d8
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-22 16:42:31 -0500

    Cleanup

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -1,74 +1,13 @@
 import { ApolloServer, gql } from "apollo-server-micro";
-import { Side } from "../../components/card-search-table/card.interface";
 import schema from "../../graphql/schema.gql";
+import { dummyDeckData } from "../../utils/dummy-deck-data";
 
 const typeDefs = gql(schema);
 
 const resolvers = {
   Query: {
     hello: (_parent, _args, _context) => "Hello!",
-    recentDecks: () => [
-      {
-        id: "1",
-        side: Side.dark,
-        title: "May the 4th be with you",
-        createdAt: new Date(),
-        author: { username: "darthvoodoo" },
-        days: 2,
-        description: "Fast paced, high stakes, fun deck",
-        averageRating: 3.5,
-      },
-      {
-        id: "2",
-        side: Side.light,
-        title: "Princess leia's legion",
-        createdAt: new Date(),
-        author: { username: "freeForce4you" },
-        days: 10,
-        description: "Slow methodical deck good against many types",
-        averageRating: 5,
-      },
-      {
-        id: "3",
-        side: Side.dark,
-        title: "PLANET DESTROYER",
-        createdAt: new Date(),
-        author: { username: "darthWillMaulYou" },
-        days: 7,
-        description: "Weapons galore",
-        averageRating: 2.5,
-      },
-      {
-        id: "4",
-        side: Side.light,
-        title: "Solo's surprise",
-        createdAt: new Date(),
-        author: { username: "Falconator" },
-        days: 15,
-        description: "Fast paced, high stakes, fun deck",
-        averageRating: 4.5,
-      },
-      {
-        id: "5",
-        side: Side.dark,
-        title: "Jar Jar's Jam",
-        createdAt: new Date(),
-        author: { username: "solo547" },
-        days: 12,
-        description: "Slow methodical deck good against many types",
-        averageRating: 3.5,
-      },
-      {
-        id: "6",
-        side: Side.dark,
-        title: "Empire",
-        createdAt: new Date(),
-        author: { username: "Iam Your Father?" },
-        days: 1,
-        description: "Solid all around deck",
-        averageRating: 3,
-      },
-    ],
+    recentDecks: () => dummyDeckData,
   },
 };
 
