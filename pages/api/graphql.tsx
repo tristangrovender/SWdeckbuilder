51b17ec3d812b16cfb72c699d249be32f99b01ec
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-22 14:23:59 -0500

    Making a recent decks resolver

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -3,12 +3,18 @@ import { ApolloServer, gql } from "apollo-server-micro";
 const typeDefs = gql`
   type Query {
     hello: String!
+    recentDecks: [Deck]!
+  }
+  type Deck {
+    id: ID!
+    title: String!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: (_parent, _args, _context) => "Hello!",
+    recentDecks: () => [{ id: "test", title: "test" }],
   },
 };
 
