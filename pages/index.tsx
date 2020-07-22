0574853c1905512166354b815d6254a4bfb24d6d
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-22 14:42:03 -0500

    Moved index deck tiles into json format

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Toolbar, Content, Page } from "../components/Toolbar";
 import styled from "styled-components";
 import { DeckTile } from "../components/DeckTile";
 import Footer from "../components/Footer";
+import { Side } from "../components/card-search-table/card.interface";
 
 const HomePageContent = styled.div`
   display: flex;
@@ -46,6 +47,57 @@ const TileContainer = styled.div`
   justify-content: space-between;
 `;
 
+const dummyDecks = [
+  {
+    side: Side.dark,
+    title: "May the 4th be with you",
+    author: "darthvoodoo",
+    days: 2,
+    description: "Fast paced, high stakes, fun deck",
+    rating: 3.5,
+  },
+  {
+    side: Side.light,
+    title: "Princess leia's legion",
+    author: "freeForce4you",
+    days: 10,
+    description: "Slow methodical deck good against many types",
+    rating: 5,
+  },
+  {
+    side: Side.dark,
+    title: "PLANET DESTROYER",
+    author: "darthWillMaulYou",
+    days: 7,
+    description: "Weapons galore",
+    rating: 2.5,
+  },
+  {
+    side: Side.light,
+    title: "Solo's surprise",
+    author: "Falconator",
+    days: 15,
+    description: "Fast paced, high stakes, fun deck",
+    rating: 4.5,
+  },
+  {
+    side: Side.dark,
+    title: "Jar Jar's Jam",
+    author: "solo547",
+    days: 12,
+    description: "Slow methodical deck good against many types",
+    rating: 3.5,
+  },
+  {
+    side: Side.dark,
+    title: "Empire",
+    author: "IamYour,Father?",
+    days: 1,
+    description: "Solid all around deck",
+    rating: 3,
+  },
+];
+
 export default function Home() {
   return (
     <Page>
@@ -58,150 +110,39 @@ export default function Home() {
             <RecentTitle>Recent decks</RecentTitle>
           </RecentTitleContainer>
           <TileContainer>
-            <DeckTile
-              img="/images/dark.png"
-              title="May the 4th be with you"
-              author="darthvoodoo"
-              days={2}
-              description="Fast paced, high stakes, fun deck"
-              rating={3}
-              cardBreakdown={{
-                objectives: 5,
-                locations: 5,
-                characters: 25,
-                creatures: 25,
-                weapons: 13,
-                devices: 13,
-                starships: 13,
-                vehicles: 13,
-                effects: 10,
-                interrupts: 7,
-                epicEvents: 13,
-                jediTests: 13,
-                admiralsOrders: 2,
-                podracers: 1
-              }}
-            />
-            <DeckTile
-              img="/images/light.png"
-              title="Princess leia's legion"
-              author="freeForce4you"
-              days={10}
-              description="Slow methodical deck good against many types"
-              rating={5}
-              cardBreakdown={{
-                objectives: 3,
-                locations: 5,
-                characters: 40,
-                creatures: 25,
-                weapons: 10,
-                devices: 13,
-                starships: 13,
-                vehicles: 13,
-                effects: 5,
-                interrupts: 2,
-                epicEvents: 13,
-                jediTests: 13,
-                admiralsOrders: 2,
-                podracers: 1
-              }}
-            />
-            <DeckTile
-              img="/images/dark.png"
-              title="PLANET DESTROYER"
-              author="darthWillMaulYou"
-              days={7}
-              description="Weapons galore"
-              rating={2.5}
-              cardBreakdown={{
-                objectives: 1,
-                locations: 5,
-                characters: 16,
-                creatures: 25,
-                weapons: 20,
-                devices: 13,
-                starships: 13,
-                vehicles: 13,
-                effects: 8,
-                interrupts: 15,
-                epicEvents: 13,
-                jediTests: 13,
-                admiralsOrders: 2,
-                podracers: 1
-              }}
-            />
-            <DeckTile
-              img="/images/light.png"
-              title="Solo's surprise"
-              author="Falconator"
-              days={15}
-              description="Fast paced, high stakes, fun deck"
-              rating={4.5}
-              cardBreakdown={{
-                objectives: 1,
-                locations: 5,
-                characters: 33,
-                creatures: 25,
-                weapons: 14,
-                devices: 13,
-                starships: 13,
-                vehicles: 13,
-                effects: 11,
-                interrupts: 1,
-                epicEvents: 13,
-                jediTests: 13,
-                admiralsOrders: 2,
-                podracers: 1
-              }}
-            />
-            <DeckTile
-              img="/images/light.png"
-              title="Jar Jar's Jam"
-              author="solo547"
-              days={12}
-              description="Slow methodical deck good against many types"
-              rating={3.5}
-              cardBreakdown={{
-                objectives: 4,
-                locations: 5,
-                characters: 39,
-                creatures: 25,
-                weapons: 11,
-                devices: 13,
-                starships: 13,
-                vehicles: 13,
-                effects: 1,
-                interrupts: 5,
-                epicEvents: 13,
-                jediTests: 13,
-                admiralsOrders: 2,
-                podracers: 1
-              }}
-            />
-            <DeckTile
-              img="/images/dark.png"
-              title="Empire"
-              author="IamYourFather?"
-              days={1}
-              description="Solid all around deck"
-              rating={3}
-              cardBreakdown={{
-                objectives: 3,
-                locations: 5,
-                characters: 33,
-                creatures: 25,
-                weapons: 12,
-                devices: 13,
-                starships: 13,
-                vehicles: 13,
-                effects: 10,
-                interrupts: 2,
-                epicEvents: 13,
-                jediTests: 13,
-                admiralsOrders: 2,
-                podracers: 1
-              }}
-            />
+            {dummyDecks.map(
+              ({ side, title, author, days, description, rating }, i) => (
+                <DeckTile
+                  key={i}
+                  img={
+                    side === Side.dark
+                      ? "/images/dark.png"
+                      : "/images/light.png"
+                  }
+                  title={title}
+                  days={days}
+                  rating={rating}
+                  description={description}
+                  author={author}
+                  cardBreakdown={{
+                    objectives: 5,
+                    locations: 5,
+                    characters: 25,
+                    creatures: 25,
+                    weapons: 13,
+                    devices: 13,
+                    starships: 13,
+                    vehicles: 13,
+                    effects: 10,
+                    interrupts: 7,
+                    epicEvents: 13,
+                    jediTests: 13,
+                    admiralsOrders: 2,
+                    podracers: 1,
+                  }}
+                />
+              )
+            )}
           </TileContainer>
         </HomePageContent>
       </Content>
