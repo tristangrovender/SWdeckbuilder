e3f3b56c03d32a3d6a355e9abf154c3520d9f532
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-22 16:55:05 -0500

    Working on returning cards as well

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -1,13 +1,26 @@
 import { ApolloServer, gql } from "apollo-server-micro";
 import schema from "../../graphql/schema.gql";
 import { dummyDeckData } from "../../utils/dummy-deck-data";
+import { Card } from "../../components/card-search-table/card.interface";
+const cards = require("../../cards/cards.json");
+
+function generateDeck(allCards: Card[]): Card[] {
+  /// TODO maake smarter
+  return allCards.slice(0, 60);
+}
 
 const typeDefs = gql(schema);
 
 const resolvers = {
   Query: {
     hello: (_parent, _args, _context) => "Hello!",
-    recentDecks: () => dummyDeckData,
+    recentDecks: () =>
+      dummyDeckData.map((deck) => {
+        return {
+          ...deck,
+          cards: generateDeck(cards),
+        };
+      }),
   },
 };
 
