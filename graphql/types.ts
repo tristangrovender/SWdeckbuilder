267c9418cdebd71948d537dee000780d88a88fd7
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-22 16:15:37 -0500

    Added yarn generate

---
+++ graphql/types.ts
@@ -0,0 +1,63 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Date: any;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  hello: Scalars['String'];
+  recentDecks: Array<Maybe<Deck>>;
+};
+
+export enum Side {
+  Dark = 'Dark',
+  Light = 'Light'
+}
+
+
+export type Deck = {
+  __typename?: 'Deck';
+  id: Scalars['ID'];
+  createdAt: Scalars['Date'];
+  side: Side;
+  title: Scalars['String'];
+  description: Scalars['String'];
+  author: User;
+  averageRating?: Maybe<Scalars['Float']>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  username: Scalars['String'];
+};
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE'
+}
+
+
+export type GetRecentDecksQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetRecentDecksQuery = (
+  { __typename?: 'Query' }
+  & { recentDecks: Array<Maybe<(
+    { __typename?: 'Deck' }
+    & Pick<Deck, 'id' | 'side' | 'title' | 'description' | 'createdAt' | 'averageRating'>
+    & { author: (
+      { __typename?: 'User' }
+      & Pick<User, 'username'>
+    ) }
+  )>> }
+);
