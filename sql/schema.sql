dbf1b624759222124103bb78534150b1ec79e327
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-23 13:33:11 -0500

    Updates to schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,20 @@
-CREATE TABLE "public"."User" (
+CREATE TABLE "public"."User"
+(
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  email VARCHAR(255) UNIQUE NOT NULL
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255) UNIQUE NOT NULL,
+  username VARCHAR(255)
+);
+
+CREATE TABLE "public"."Deck"
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  title VARCHAR(255),
+  side VARCHAR(255),
+  description VARCHAR(8000),
 );
