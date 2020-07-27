a52cb4dbe59833447fa89bf4644b14f321844569
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-27 11:22:47 -0500

    Added mutation for creating a deck

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -2,6 +2,7 @@ import { ApolloServer, gql } from "apollo-server-micro";
 import schema from "../../graphql/schema.gql";
 import { PrismaClient } from "@prisma/client";
 import { recentDecks } from "../../server/resolvers/recent-decks";
+import { getSharedUser } from "../../server/create-shared-user";
 
 const prisma = new PrismaClient();
 
@@ -11,6 +12,21 @@ const resolvers = {
   Query: {
     hello: (_parent, _args, _context) => "Hello!",
     recentDecks: () => recentDecks(prisma),
+  },
+  Mutation: {
+    createDeck: async (_parent, _args) => {
+      const user = await getSharedUser(prisma);
+      return prisma.deck.create({
+        data: {
+          side: _args.side,
+          User: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      });
+    },
   },
 };
 
