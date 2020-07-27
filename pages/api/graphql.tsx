8fe30d6c4e3cb1fb340816dc21749b667120f4b9
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-27 11:57:02 -0500

    require loging to create a deck

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -24,14 +24,16 @@ const resolvers = {
         jwt: jwt.sign({ userId: user.id }, jwtSecret),
       };
     },
-    createDeck: async (_parent, _args) => {
-      const user = await getSharedUser(prisma);
+    createDeck: async (_parent, _args, _context) => {
+      if (!_context.userId) {
+        throw new Error("Please login");
+      }
       return prisma.deck.create({
         data: {
           side: _args.side,
           User: {
             connect: {
-              id: user.id,
+              id: _context.userId,
             },
           },
         },
