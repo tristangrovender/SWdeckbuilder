7d94324149cc1d3e22084a1b424737261e506615
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-27 17:58:40 -0500

    Updated card page

--- pages/card/[id].tsx
+++ pages/card/[id].tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import { Page, Toolbar, Content } from "../../components/Toolbar";
 import { useRouter } from "next/router";
-import { Card } from "../../components/card-search-table/card.interface";
-import { getCards } from "../../components/card-search-table/getCards";
+import { getCardsFromServer } from "../../components/card-search-table/getCards";
 import { CommentsSection } from "../../components/comments-section";
 import Footer from "../../components/Footer";
+import { Card } from "../../graphql/types";
 
 export default function CardPage(params) {
   const router = useRouter();
   const [cards, setCards] = useState(null);
   if (cards === null) {
-    getCards().then(setCards);
+    getCardsFromServer().then(setCards);
     return (
       <Page>
         <Toolbar />
@@ -41,16 +41,16 @@ export default function CardPage(params) {
     <Page>
       <Toolbar />
       <Content>
-        <h2>{card.front.title}</h2>
+        <h2>{card.title}</h2>
         <div style={{ display: "flex" }}>
           <div>
             <div style={{ marginBottom: "30px" }}>
-              {card.side}: {card.front.type} - {card.front.subType}
+              {card.side}: {card.type} - {card.subType}
             </div>
-            <div style={{ fontStyle: "italic" }}>{card.front.gametext}</div>
+            <div style={{ fontStyle: "italic" }}>{card.gametext}</div>
             <CommentsSection></CommentsSection>
           </div>
-          <img src={card.front.imageUrl} style={{ height: "500px" }}></img>
+          <img src={card.imageUrl} style={{ height: "500px" }}></img>
         </div>
       </Content>
       <Footer></Footer>
