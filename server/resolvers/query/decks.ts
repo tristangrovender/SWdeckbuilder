8f6d5e2b5b70dc185d2179d3985d9f174eb85a94
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-07-30 11:21:23 -0500

    Fixes

--- server/resolvers/query/decks.ts
+++ server/resolvers/query/decks.ts
@@ -11,20 +11,23 @@ export function decks(
   _args: RequireFields<QueryDecksArgs, "authorId">,
   _context: Context
 ) {
-  if (_args.authorId !== _context.userId) {
-    throw new Error("You can only query for your own decks");
-  }
   if (_args.authorId) {
-    return prisma.deck.findMany({
-      orderBy: {
-        updated_at: "desc",
-      },
-      where: {
-        User: {
-          id: parseInt(_args.authorId),
-        },
-      },
-    });
+    if (parseInt(_args.authorId) !== _context.userId) {
+      throw new Error("You can only query for your own decks");
+    } else {
+      if (_args.authorId) {
+        return prisma.deck.findMany({
+          orderBy: {
+            updated_at: "desc",
+          },
+          where: {
+            User: {
+              id: parseInt(_args.authorId),
+            },
+          },
+        });
+      }
+    }
   }
   return prisma.deck.findMany({
     orderBy: {
