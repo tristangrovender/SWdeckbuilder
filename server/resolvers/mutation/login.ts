b5960969d8d0b4061b67e4c6c52189cef2177027
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-08-05 14:56:40 -0500

    Got token validation somewhat working

--- server/resolvers/mutation/login.ts
+++ server/resolvers/mutation/login.ts
@@ -1,10 +1,14 @@
 import jwt from "jsonwebtoken";
 import { getSharedUser } from "../../create-shared-user";
 import { prisma } from "../../../pages/api/graphql";
+import { handler } from "../../decode-verify-jwt";
 
-export async function login() {
+export async function login(_parent, _args) {
+  const result = await handler({ token: _args.awsJWTToken });
+  console.log("result!", result);
+
   const user = await getSharedUser(prisma);
-  console.log(process.env.JWT_SECRET);
+  // console.log(process.env.JWT_SECRET);
   return {
     jwt: jwt.sign({ userId: user.id }, process.env.JWT_SECRET),
   };
