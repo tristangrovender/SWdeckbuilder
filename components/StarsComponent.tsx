99aa6e9ba165f094cd4c3c62a8332fb9a03dfa58
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-08-06 13:31:09 -0500

    Progress

--- components/StarsComponent.tsx
+++ components/StarsComponent.tsx
@@ -2,6 +2,7 @@ import StarsRating from "stars-rating";
 import { RatingText } from "./DeckTile";
 import { average } from "../utils/utils";
 import { Maybe } from "../graphql/types";
+import { getToken } from "../utils/frontend-auth";
 export function StarsComponent({
   ratings,
   onChange,
@@ -12,13 +13,14 @@ export function StarsComponent({
   const scores = ratings
     .map((rating) => rating?.rating)
     .filter((rating) => rating !== null && rating !== undefined) as number[];
+  const isLoggedIn = Boolean(getToken());
   return (
     <>
       <StarsRating
         count={5}
         size={15}
         color2={"#ffd700"}
-        edit={Boolean(onChange)}
+        edit={Boolean(onChange) && isLoggedIn}
         value={average(scores)}
         half={true}
         onChange={onChange}
