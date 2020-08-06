7f1da88762a344b4d188764fa77849a27ac34382
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-08-06 18:01:57 -0500

    Adding prod login

--- utils/frontend-auth.ts
+++ utils/frontend-auth.ts
@@ -13,8 +13,31 @@ export function setToken(token: string) {
 export function removeToken() {
   return localStorage.removeItem(JWT_KEY);
 }
+enum Environment {
+  prod = "Prod",
+  local = "Local",
+}
+function getEnvironment(): Environment {
+  try {
+    if (window.location.origin.includes("localhost")) {
+      return Environment.local;
+    } else {
+      return Environment.prod;
+    }
+  } catch {
+    return Environment.local;
+  }
+}
 export function getSignInUrl() {
-  return "https://swccgdb.auth.us-east-2.amazoncognito.com/login?client_id=4ab8m6p1nglpa4110o2jhpk17&response_type=token&scope=email+openid+profile&redirect_uri=http://localhost:3000/callback";
+  const developmentUrl =
+    "https://swccgdb.auth.us-east-2.amazoncognito.com/login?client_id=4ab8m6p1nglpa4110o2jhpk17&response_type=token&scope=email+openid+profile&redirect_uri=http://localhost:3000/callback";
+  const productionUrl =
+    "https://swccgdb-production.auth.us-east-2.amazoncognito.com/login?client_id=3hganbcfgre49cutjo2acf2kv0&response_type=token&scope=email+openid+profile&redirect_uri=https://sw-deckbuilder.danielrasmuson.vercel.app/callback";
+  if (getEnvironment() === Environment.prod) {
+    return productionUrl;
+  } else {
+    return developmentUrl;
+  }
 }
 export function getUserId() {
   const token = getToken();
