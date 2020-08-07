8213f847d0c97e62d5dbef223553d1230b8c38a0
Daniel Rasmuson <danielrasmuson@gmail.com>
2020-08-07 10:23:26 -0500

    Adding deploy commands

---
+++ deploy.js
@@ -0,0 +1,19 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+(async () => {
+  const { stdout } = await exec("doppler enclave secrets --config prd --json");
+
+  const envVars = JSON.parse(stdout);
+
+  const deployCmd = Object.entries(envVars).reduce(
+    (command, [envName, { computed: envValue }]) => {
+      return `${command} -e ${envName}="${envValue}"`;
+    },
+    "now"
+  );
+
+  const { stdout, stderr } = await exec(deployCmd);
+
+  console.log(stdout, stderr);
+})();
